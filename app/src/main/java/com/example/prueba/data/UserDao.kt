55c9fd89ce044d2cd0efde69578311d3f74e9fc4
Jolgo <jlgo60@hotmail.com>
2020-11-28 16:13:26 -0500

    se agrego esquema DB

---
+++ app/src/main/java/com/example/prueba/data/UserDao.kt
@@ -0,0 +1,18 @@
+package com.example.prueba.data
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface UserDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun addUser(user: User)
+
+    @Update
+    suspend fun updateUser(user: User)
+
+    @Query("SELECT * FROM user_table ORDER BY id ASC")
+    fun readAllData(): LiveData<List<User>>
+
+}
---
+++ app/src/main/java/com/example/prueba/data/UserRepository.kt
@@ -0,0 +1,17 @@
+package com.example.prueba.data
+
+import androidx.lifecycle.LiveData
+
+class UserRepository(private val userDao: UserDao) {
+
+    val readAllData: LiveData<List<User>> = userDao.readAllData()
+
+    suspend fun addUser(user: User){
+        userDao.addUser(user)
+    }
+
+    suspend fun updateUser(user: User){
+        userDao.updateUser(user)
+    }
+
+}
